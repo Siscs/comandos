69d28f25fb53f7a9070a16af60b493d307ccdc24
Siscs <37455714+Siscs@users.noreply.github.com>
2023-06-30 10:26:56 -0300

    Update coverlet.bat

--- coverlet.bat
+++ coverlet.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Pacote necessario coverlet.msbuild nos projetos de testes
 REM dotnet tool install -g dotnet-reportgenerator-globaltool
-cd C:\Dados\Dotz\Projetos\cashback
+cd C:\Dados\Projetos\projetoX
 dotnet test -p:CollectCoverage=true -p:CoverletOutputFormat=cobertura -p:CoverletOutput=c:\dados\tests\projetox\
 reportgenerator -reports:".\projeto.Tests\coverage.cobertura.xml" -targetdir:"c:\dados\temp\coverage" -reporttypes:Html
 start c:\dados\temp\coverage\index.html
