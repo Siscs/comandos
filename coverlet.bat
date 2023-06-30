95b622b45902e5a2340292caec04f715638c4ed1
Siscs <37455714+Siscs@users.noreply.github.com>
2023-06-30 10:25:40 -0300

    Update coverlet.bat

--- coverlet.bat
+++ coverlet.bat
@@ -1,7 +1,8 @@
 @echo off
-REM Pacote necessario coverlet.msbuild  Version=3.1.2 no projeto de testes
+REM Pacote necessario coverlet.msbuild nos projetos de testes
+REM dotnet tool install -g dotnet-reportgenerator-globaltool
 cd C:\Dados\Dotz\Projetos\cashback
-dotnet test -p:CollectCoverage=true -p:CoverletOutputFormat=cobertura
-reportgenerator -reports:".\Cashback.Tests\coverage.cobertura.xml" -targetdir:"c:\dados\temp\coverage" -reporttypes:Html
+dotnet test -p:CollectCoverage=true -p:CoverletOutputFormat=cobertura -p:CoverletOutput=c:\dados\tests\projetox\
+reportgenerator -reports:".\projeto.Tests\coverage.cobertura.xml" -targetdir:"c:\dados\temp\coverage" -reporttypes:Html
 start c:\dados\temp\coverage\index.html
 cd c:\dados
